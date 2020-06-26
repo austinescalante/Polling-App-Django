84dea8cc08231fe6cecb1df2e0166a11e5811c8e
austinescalante <60208327+austinescalante@users.noreply.github.com>
2020-06-26 11:36:30 -0700

    Bootstrap and Question View

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Question, Choice
+
+#Get Questions and Display them
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #pass in as dictionary
+    context = {'latest_question_list':latest_question_list}
+    return render(request,'polls/index.html',context)
+
+#Show question and choice
+def detail(request, question_id):
+    try:
+        question = Question.objects.Get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/results.html', { 'question': question})
+
+#Get question and display results
+def results(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request, 'polls/results.html',{ 'question': question})
